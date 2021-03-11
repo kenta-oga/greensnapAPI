c3f3676579f167c619ede81d2e9a8c28ddde1fd0
kenta-oga <kenta555phi@gmail.com>
2021-03-12 03:17:55 +0900

    不要な記述削除

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,6 +1,4 @@
 class SamplesController < ApplicationController
-  before_action :set_sample, only: [:show, :update, :destroy]
-  require 'csv'
   require "json"
 
   def change
@@ -16,52 +14,4 @@ class SamplesController < ApplicationController
     @str = JSON.generate(@datas)
     render xml: @str
   end
-
-  # GET /samples
-  def index
-    @samples = Sample.all
-
-    render json: @samples
-  end
-
-  # GET /samples/1
-  def show
-    render json: @sample
-  end
-
-  # POST /samples
-  def create
-    @sample = Sample.new(sample_params)
-
-    if @sample.save
-      render json: @sample, status: :created, location: @sample
-    else
-      render json: @sample.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /samples/1
-  def update
-    if @sample.update(sample_params)
-      render json: @sample
-    else
-      render json: @sample.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /samples/1
-  def destroy
-    @sample.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sample
-      @sample = Sample.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def sample_params
-      params.require(:sample).permit(:user_name, :comment)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :samples do
+  resources :samples, only: [] do
     collection do
       get 'change'
     end
