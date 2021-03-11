aa3f6b53c0845b9b276f94fb8cc01e35e969a584
kenta-oga <kenta555phi@gmail.com>
2021-03-12 03:56:34 +0900

    jsonファイルで出力

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,16 +2,15 @@ class SamplesController < ApplicationController
   require "json"
 
   def change
-    @datas = []
     @samples = File.open("#{Rails.public_path}/json/data.json") do |j|
       JSON.load(j)
     end
+    @datas = []
     @samples.each do |sample|
       user_name = sample["user_name"] + "さん"
       comment = sample["comment"].truncate(20, omission: '')
       @datas << {user_name: user_name, comment: comment} 
     end
-    @str = JSON.generate(@datas)
-    render xml: @str
+    render json: @datas
   end
 end
